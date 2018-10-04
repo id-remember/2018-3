54a5f5bbc40b656bdddbbbd4430a27c3541cd5dd
D7701 <D7701@DESKTOP-ODBNK9S>
2018-10-04 14:58:35 +0900

    컨트롤러 설정 연습

--- exBean/src/exBean/studentDAO.java
+++ exBean/src/exBean/studentDAO.java
@@ -1,5 +1,6 @@
 package exBean;
 import java.sql.*;
+import java.util.*;
 public class studentDAO {
  public void insertStudent(studentVO item) throws Exception {
   String no = item.getNo();
@@ -16,4 +17,23 @@ public class studentDAO {
   pstmt.close();
   conn.close();
  }
+ public List<studentVO> selectStudent() throws Exception {
+	  List<studentVO> items = new ArrayList<studentVO>();
+	  Class.forName("oracle.jdbc.driver.OracleDriver");
+	  Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","201800001","201800001");
+	  PreparedStatement pstmt = conn.prepareStatement("select * from student");
+	  ResultSet rs = pstmt.executeQuery();
+	  while(rs.next()) {
+	   studentVO item = new studentVO();
+	   item.setNo(rs.getString("no"));
+	   item.setName(rs.getString("name"));
+	   items.add(item);
+	   //System.out.print(items);
+	  }
+
+	  pstmt.close();
+	  rs.close();
+	  conn.close();
+	  return items;
+	 }
 }
